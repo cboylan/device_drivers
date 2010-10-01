de42c64bbf9316df480ce086b84c05165ec34c8d
Clark Boylan <clark.boylan@gmail.com>
2010-10-01 16:21:45 -0700

    Add hello world module.

---
+++ hello_world/hello_world.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init my_init (void)
+{
+    printk(KERN_INFO "Hello: module loaded at 0x%p\n", my_init);
+    return 0;
+}
+
+static int __init my_exit (void)
+{
+    printk(KERN_INFO "Hello: module unloaded from 0x%p\n", my_exit);
+}
+
+module_init (my_init);
+module_exit (my_exit);
+
+MODULE_AUTHOR ("Clark Boylan");
+MODULE_LICENSE ("GPL v2");
